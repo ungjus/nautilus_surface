25754581bd06e0ca0f4483fb800248bff5816199
wang-4 <wang4@cs.washington.edu>
2021-11-10 14:52:53 -0800

    Added code for webcam const, made some updates to existing code

--- src/uwrov_interface/src/components/webcam/Webcam.js
+++ src/uwrov_interface/src/components/webcam/Webcam.js
@@ -1,20 +1,54 @@
 import React from "react";
+import Webcam from "react-webcam";
+// https://www.npmjs.com/package/react-webcam
+const WebcamComponent = () => <Webcam />;
+
+const videoConstraints = {
+  width: 1280,
+  height: 720,
+  facingMode: "user"
+};
+
+const WebcamCapture = () => {
+  const webcamRef = React.useRef(null);
+
+  const capture = React.useCallback(
+    () => {
+      const imageSrc = webcamRef.current.getScreenshot();
+    },
+    [webcamRef]
+  );
+
+  return (
+    <>
+      <Webcam
+        audio={true}
+        height={720}
+        ref={webcamRef}
+        screenshotFormat="image/jpeg"
+        width={1280}
+        videoConstraints={videoConstraints}
+      />
+      <button onClick={capture}>Capture photo</button>
+    </>
+  );
+};
 
 export default class Webcam extends React.Component {
   constructor(props) {
     super(props);
-    // this.socket = require('socket.io-client')('http://localhost:4040')
+    this.socket = require('socket.io-client')('http://localhost:4040')
     this.state = { //never set the state directly, just use setState. also, setState is asynch 
-      text: "Placeholder text"
-      //frame
+      text: "Placeholder text",
+      frame: getScreenshot()
     };
 
     // this.socket.on("Text", this.updateText);
   }
 
-  updateText = (data) => {
-    console.log("got text update: " + data.text);
-    this.setState({ text: data.text });
+  updateCam = (data) => {
+    console.log("got camera update");
+    this.setState({ frame: data.frame });
   }
 
   render() { //things it'll display 
