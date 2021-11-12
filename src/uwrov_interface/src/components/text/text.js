ef0527217969486d6b79e7374b19c9c58f9941d8
amalj3 <93282976+amalj3@users.noreply.github.com>
2021-11-11 16:14:07 -0800

    Add files via upload

---
+++ src/uwrov_interface/src/components/text/text.js
@@ -0,0 +1,31 @@
+import React from "react";
+import MessageForm from './MessageForm'
+import MessageList from './MessageList'
+
+export default class Text extends React.Component {
+  constructor(props) {
+    super(props);
+    //this.socket = require('socket.io-client')('http://localhost:4040')
+    this.state = {
+      messages: [],
+    }
+
+    //this.socket.on("Text", this.updateText);
+  }
+
+  handleNewMessage = (text) => {
+    console.log("got text update: " + text);
+    this.setState({
+      messages: [...this.state.messages, { me: true, author: "Me", body: text }],
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <MessageList messages={this.state.messages} />
+        <MessageForm onMessageSend={this.handleNewMessage} />
+      </div>
+    );
+  }
+}
